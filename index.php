a216f65df84e5335f149bc043e607a5850febced
CyberXBD <144501443+CyberXBD@users.noreply.github.com>
2023-09-09 09:52:13 +0600

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">    
-    <title>Motu Patlu</title>
+    <title>CYBERXBD</title>
 
     <link href="https://fonts.googleapis.com/css?family=Muli:300,300i,400,400i,600,600i,700,700i%7CComfortaa:300,400,700" rel="stylesheet">
     <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
@@ -123,7 +123,7 @@
 		<div class="col-md-8">
 			<div class="card">
 				<div class="card-body text-center">
-					<h4 class="mb-2"><strong>Motu Patlu</strong></h4>
+					<h4 class="mb-2"><strong>cyberxbd</strong></h4>
 
 					<textarea rows="12" class="form-control text-center form-checker mb-2" placeholder="PUT _YOUR_CARDS_HERE"></textarea>
 					<textarea rows="1" class="form-control text-center"style="width:80%; float: left ;" id="sec" placeholder="TeleGram ID"></textarea>
@@ -269,7 +269,7 @@ swalWithBootstrapButtons.fire({
 })
 
 function RedirectUrl(){    
-       window.location.href ="https://t.me/MotuSamusa";
+       window.location.href ="https://t.me/cyberxbd";
     }
     
 
